c4de0726e0e349cf0385f785b26d22b154786eed
ageolena <ageolena@gmail.com>
2024-03-19 08:57:37 -0400

    button component

---
+++ src/components/button/button.tsx
@@ -0,0 +1,45 @@
+import { Alarm, Delete, Fingerprint, Home } from '@mui/icons-material';
+import { IconButton, Button as MUIButton } from '@mui/material'
+import { FC, ReactNode } from 'react';
+
+type IconType = 'fingerprint' | 'alarm' | 'home' | 'delete';
+
+type ButtonProps = {
+    variant?: 'text' | 'outlined' | 'contained' | 'icon';
+    icon?: IconType;
+    size?: 'small' | 'medium' | 'large';
+    options?: any;
+    children?: ReactNode;
+}
+
+export const Button: FC<ButtonProps> = ({
+    variant = 'contained',
+    icon = 'home',
+    size = 'medium',
+    options = {},
+    children = null,
+    ...rest
+}) => {
+
+    const Icon = () =>{
+        switch (icon) {
+            case 'fingerprint': return <Fingerprint fontSize='inherit' />;
+            case 'alarm': return <Alarm fontSize='inherit' />;
+            case 'home': return <Home fontSize='inherit' />;
+            case 'delete': return <Delete fontSize='inherit' />;
+            default: return <Home fontSize='inherit' />;
+        }
+    }
+
+    const buttonAttributes = { size, ...options, ...rest }
+
+    return variant === 'icon' ?
+        <IconButton variant='icon' {...buttonAttributes} aria-label={icon || 'icon-button'}>
+            {children || <Icon />}
+        </IconButton>
+        : <MUIButton variant={variant} {...buttonAttributes}>
+            {children || 'Click me!'}
+        </MUIButton>
+}
+
+export default Button;
